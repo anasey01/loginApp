53206d52f31be295264f3d416451ef73c4e0a695
Oluseyi Anani <anasey2018@gmail.com>
2018-07-27 00:03:20 +0100

    Required neccessary Modules

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var express = require("express"),
-    
+    path    = require("path"),
+    cookieParser = require("cookie-parser"),
+    bodyParser  = require("body-parser"),
+    exphbs      = require("express-handlebars"),
+    expressValidator = require("express-validator"),
+    flash              = require("connect-flash"),
+    session         = require("express-session"),
+    passport        = require("passport"),
+    LocalStragegy   = require("passport-local").Strategy,
+    mongo           = require("mongodb"),
+    mongoose        = require("mongoose");
+
+mongoose.connect('mongodb://localhost/loginApp');
+var db = mongoose.connection;
